57e01ffd0fbd9406706b6388a9380f7b477ab72d
GauravMendix0 <gaurav.gokhale@mendix.com>
2025-06-08 11:26:31 -0700

    pushing first codes

---
+++ overloading.java
@@ -0,0 +1,31 @@
+class demo
+{
+    int x=10;
+    public void show()
+    {
+        System.out.println("In a show");
+    }
+
+    public void show(int num)
+    {
+        System.out.println("Now in show int");
+    }
+}
+
+class demo2 extends demo
+{
+    public void show()
+    {
+        System.out.println("In the class demo2 show");
+    }
+}
+
+public class overloading {
+    public static void main(String[] args) {
+        demo2 d=new demo2();
+
+        d.show();
+
+
+    }
+}
